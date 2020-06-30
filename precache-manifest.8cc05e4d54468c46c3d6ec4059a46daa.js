955e7014b05ae8e1b4f462866479a5bcb050a66d
vmeset <vmeset@gmail.com>
2020-06-30 10:33:15 +0300

    Updates

---
+++ precache-manifest.8cc05e4d54468c46c3d6ec4059a46daa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9e4363f79325d63bc9f7bebbb983d2d",
+    "url": "/tr2/index.html"
+  },
+  {
+    "revision": "59274f129fa958f03604",
+    "url": "/tr2/static/css/main.346f5fc9.chunk.css"
+  },
+  {
+    "revision": "738c5565e6cf733be429",
+    "url": "/tr2/static/js/2.b4ec797b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tr2/static/js/2.b4ec797b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59274f129fa958f03604",
+    "url": "/tr2/static/js/main.b0db48cd.chunk.js"
+  },
+  {
+    "revision": "7b96145d29f12b50f574",
+    "url": "/tr2/static/js/runtime-main.abf16b5a.js"
+  },
+  {
+    "revision": "9064981b80e59f1b663c17fab41563c9",
+    "url": "/tr2/static/media/Spinner-1s-200px.9064981b.svg"
+  }
+]);
